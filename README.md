650c41eacbfb7aae5caad5fc7072da75c2c4ba47
Emmanuel <github-sub@shedge.com>
2023-03-28 18:34:23 +0200

    feature: first version of the Prefect pipeline with Web to GCS deployement

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
 # DE_bootcamp_project
+
+## Data
+
+- https://odre.opendatasoft.com/explore/dataset/temperature-quotidienne-regionale
+- https://odre.opendatasoft.com/explore/dataset/stock-quotidien-stockages-gaz/
+- https://odre.opendatasoft.com/explore/dataset/eco2mix-national-tr
+
+## Install PREFECT and other libs
+
+>>> python -m venv venvProject
+>>> source venvProject/bin/activate
+>>> pip install -r requirements.txt
+
+>>> prefect version
+
+## Run local PREFECT GUI
+
+>>> prefect orion start
+open http://127.0.0.1:4200/
+
+
+
+## --------------------------------------------------------------
+## Do ETL and then send the resulting file to Google Cloud Storage (gcs)
+
+we write a whole new script (see 02_gcp)
+
+### Let's create a bucket to store the project files
+
+- Go to GCP-UI // "Cloud Storage" / "Buckets" / "Create"
+
+name --> de_project_gcp_bucket
+data location type --> europe-west1(Belgium)
+data storage class --> standard (Best for short-term storage and frequently accessed data)
+
+- Click on "CREATE"
+
+### We can see the bucket content
+
+- Go to GCP-UI // "Cloud Storage" / "Buckets" / "de_project_gcp_bucket"
+
+### Let's create an account with the appropriate rights 
+
+- Go to GCD-UI // "IAM & Admin" / "Service Account" / "Create service account"
+
+-->1/ service account-name: PROJECT-NAME-user-bucket (de_project_user_bucket)
+-->2/ role: "BigQuery Admin" & "Cloud Storage / Storage Admin"
+--> Save
+-->3/ .
+--> Done
+
+### Let's create a remote key for the account
+
+- Go to GCD-UI // "IAM & Admin" / "Service Account"
+- Click '...' and "Manage keys" on the appropriate service account (de_project_user_bucket)
+- Go to "Add Key" / "Create new key" / "JSON" --> A json file is saved to the computer
+
+### Let's add the PREFECT GCP Block
+
+Register blocks types within a module or file.
+                                                                                                                                                                                   
+#### Make sure the targeted blocks is available for configuration via the UI. (If a block type has already been registered, its registration will be updated to match the block's current definition)
+
+>>> prefect block register -m prefect_gcp 
+
+#### PREFECT-GUI ## "Blocks" / "Add Block" / "GCS Bucket"
+
+name --> de-project-user-bucket
+bucket-name --> de_project_gcp_bucket
+
+#### Click "ADD" on GCP-Credentials
+
+name --> de-project-gcs-creds
+service account info --> copy the content of the JSON file associated with the bucket account (we downloaded it earlier)
+
+--> Done
+--> Select the newly created  GCP credential in the select box
+--> Create
+
+
+#### Edit script
+==> Add GcsBucket (as explained on the PREFECT-GUI GcSBucket block page)
+
+
+
---
+++ flows/etl_web_to_gcs.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python3
+
+import os
+import pathlib
+import pandas as pd
+from datetime import timedelta
+
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect.tasks import task_input_hash
+
+
+@task(
+    name="Extract Data",
+    log_prints=True,
+    retries=3,
+    cache_key_fn=task_input_hash,
+    cache_expiration=timedelta(days=1),
+)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read data from web into pandas DataFrame"""
+
+    print("FETCH", dataset_url)
+    return pd.read_csv(dataset_url)
+
+
+@task(name="Transform Data", log_prints=True)
+def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+
+    print(df.head(2))
+
+    # print(f"pre: columns types: \n{df.dtypes}")
+    # if "lpep_pickup_datetime" in df.columns:
+    #     df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    #     df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    # elif "tpep_pickup_datetime" in df.columns:
+    #     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    #     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    # print(f"post: columns types: \n{df.dtypes}")
+
+    # print(f"pre: missing passenger count: {df[df.passenger_count == 0].shape[0]} / {df.shape[0]}")
+    # df = df[df.passenger_count != 0]
+    # print(f"post: missing passenger count: {df[df.passenger_count == 0].shape[0]} / {df.shape[0]}")
+
+    return df
+
+
+@task(name="Write Data Locally", log_prints=True)
+def write_local(df: pd.DataFrame, dataset_file: str) -> pathlib.Path:
+    """Write the DataFrame out locally as a Parquet file"""
+
+    path = pathlib.Path("data")
+    if not path.exists():
+        os.makedirs(path)
+
+    path = pathlib.Path(path, f"{dataset_file.replace('.csv','.parquet')}")
+    df.to_parquet(path, compression="gzip")
+
+    return path
+
+
+@task(name="Write Data on GCS", log_prints=True)
+def write_GCS(path: pathlib.Path) -> None:
+    """Copy the Parquet file to GCS"""
+
+    gcs_block = GcsBucket.load("de-project-user-bucket")
+    gcs_block.upload_from_path(from_path=path, to_path=path)
+
+
+@flow(log_prints=True, retries=3)
+def etl_web_to_gcs(dataset_url, dataset_file) -> None:
+    """The main ETL function"""
+
+    # dataset_file = f"{color}_tripdata_{year}-{month:02}.csv.gz"
+    # dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}"
+
+    df = fetch(dataset_url)
+    df_clean = transform_data(df)
+    path = write_local(df_clean, dataset_file)
+    write_GCS(path)
+
+
+@flow(log_prints=True)
+def etl_parent_flow() -> None:
+
+    """The main function"""
+    with open(pathlib.Path("data-sources.txt"), "r") as f:
+        for data_src in f.readlines():
+            dataset_file, dataset_url = data_src.split(' ')
+            print("ETL for :", dataset_file, dataset_url)
+            etl_web_to_gcs(dataset_url, dataset_file)
+
+
+if __name__ == "__main__":
+    etl_parent_flow()
